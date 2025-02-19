0a4e89ea95c4860871ef350e8281309bdc151ee8
EvyOksi <landonpr@icloud.com>
2025-02-19 14:42:44 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,26 +1,7 @@
-// Function to update line numbers
-function updateLineNumbers() {
-  const codeInput = document.getElementById('codeInput');
-  const lineNumbers = document.getElementById('line-numbers');
-  const lines = codeInput.value.split('\n').length;
-
-  let lineNumbersHTML = '';
-  for (let i = 1; i <= lines; i++) {
-    lineNumbersHTML += i + '<br>';
-  }
-  lineNumbers.innerHTML = lineNumbersHTML;
-}
-
-// Update line numbers on user input
-document.getElementById('codeInput').addEventListener('input', updateLineNumbers);
-
-// Initial call to set line numbers
-updateLineNumbers();
-
-// Lint button functionality
 document.getElementById('lintButton').addEventListener('click', function () {
   const code = document.getElementById('codeInput').value;
-  fetch('/lint', {
+
+  fetch('/api/lint', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
